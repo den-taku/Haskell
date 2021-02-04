f34376d72aefbbd746c57dd2b04e1ad724f9cf00
DenTaku <dentaku1412@gmail.com>
2021-02-05 07:32:45 +0900

    First Step

--- Chapter02/firstStep/app/Main.hs
+++ Chapter02/firstStep/app/Main.hs
@@ -3,4 +3,21 @@ module Main where
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+    print $ head [1, 2, 3, 4, 5]
+    print $ tail [1, 2, 3, 4, 5]
+    print $ [1, 2, 3, 4, 5] !!  2
+    print $ take 3 [1, 2, 3, 4, 5]
+    print $ drop 3 [1, 2, 3, 4, 5]
+    print $ length [1, 2, 3, 4, 5]
+    print $ sum [1, 2, 3, 4, 5]
+    print $ product [1, 2, 3, 4, 5]
+    print $ [1, 2, 3] ++ [4, 5]
+    print $ reverse [1, 2, 3, 4, 5]
+    print $ quadruple 10
+    print $ take (double 2) [1, 2, 3, 4, 5]
+    print $ factorial 10
+    print $ average [1, 2, 3, 4, 5]
+    print $ my_last [1, 2, 3, 4, 5]
+    print $ my_init1 [1, 2, 3, 4, 5]
+    print $ my_init2 [1, 2, 3, 4, 5]
--- Chapter02/firstStep/src/Lib.hs
+++ Chapter02/firstStep/src/Lib.hs
@@ -1,6 +1,21 @@
 module Lib
-    ( someFunc
+    ( someFunc, double, quadruple, factorial, average, 
+      my_last, my_init1, my_init2
     ) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+double x = x + x
+quadruple x = double (double x)
+
+-- Factorial of a positive integer
+factorial n = product [1..n]
+
+-- Average of a list of integers
+average ns = sum ns `div` length ns
+
+my_last xs = xs !! (length xs - 1)
+
+my_init1 xs = take (length xs - 1) xs
+my_init2 xs = reverse (tail (reverse xs))
