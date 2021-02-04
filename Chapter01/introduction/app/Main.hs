d4f3bd1dcbe25636237a0e83282c43eca79d60f9
DenTaku <dentaku1412@gmail.com>
2021-02-04 21:33:18 +0900

    Learn chapter01

--- Chapter01/introduction/app/Main.hs
+++ Chapter01/introduction/app/Main.hs
@@ -3,4 +3,13 @@ module Main where
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do 
+    print $ double 4
+    print $ double $ double 3
+    print $ sum [1..9]
+    print $ qsort [3, 5, 1, 4, 2]
+    print $ qsort "Hello, world!"
+    print $ my_product [2, 3, 4]
+    print $ qsort2 [3, 5, 1, 4, 2]
+    -- let str = seqn [getChar, getChar, getChar]
+    -- print str
--- Chapter01/introduction/src/Lib.hs
+++ Chapter01/introduction/src/Lib.hs
@@ -1,6 +1,32 @@
 module Lib
-    ( someFunc
+    ( someFunc, double, qsort, seqn, my_product, qsort2
     ) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+double x = x + x
+
+-- sum []     = 0
+-- sum (n:ns) = n + sum ns
+
+qsort []     = []
+qsort (x:xs) = qsort smaller ++ [x] ++ qsort larger
+               where
+                   smaller = [a | a <- xs, a <= x]
+                   larger  = [b | b <- xs, b > x]
+
+qsort2 []     = []
+qsort2 (x:xs) = qsort2 larger ++ [x] ++ qsort2 smaller
+               where
+                   smaller = [a | a <- xs, a <= x]
+                   larger  = [b | b <- xs, b > x]
+
+seqn :: Monad m => [m a] -> m [a]
+seqn []         = return []
+seqn (act:acts) = do x <- act 
+                     xs <- seqn acts
+                     return (x:xs)
+
+my_product []     = 1
+my_product (x:xs) = x * my_product xs
