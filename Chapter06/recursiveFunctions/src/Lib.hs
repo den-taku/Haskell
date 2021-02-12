8cc4e0e3f0cd4e27d365cf6672042795bee486b4
DenTaku <dentaku1412@gmail.com>
2021-02-12 20:36:03 +0900

    Done 6.2

--- Chapter06/recursiveFunctions/src/Lib.hs
+++ Chapter06/recursiveFunctions/src/Lib.hs
@@ -3,4 +3,35 @@ module Lib
     ) where
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do 
+    print $ fac 9
+    print $ 6 Lib.* 9
+    print $ Lib.product [2, 3, 4]
+    print $ Lib.reverse [1, 2, 3]
+    print $ insert 3 [1, 2, 3, 4, 5]
+    print $ isort [3,2,5,3,5,6,34,6,3,2,42,5,64,2,1,23,4,6,45,32]
+
+fac :: Int -> Int
+fac 0 = 1
+fac n = n Prelude.* fac (n-1)
+
+(*) :: Int -> Int -> Int
+m * 0 = 0
+m * n = m + (m Lib.* (n - 1))
+
+product :: Num a => [a] -> a
+product [] = 1
+product (n:ns) = n Prelude.* Lib.product ns 
+
+reverse :: [a] -> [a]
+reverse [] = []
+reverse (x:xs) = Lib.reverse xs ++ [x]
+
+insert :: Ord a => a -> [a] -> [a]
+insert x []                 = [x]
+insert x (y:ys) | x <= y    = x : y : ys
+                | otherwise = y : insert x ys
+
+isort :: Ord a => [a] -> [a]
+isort []     = []
+isort (x:xs) = insert x (isort xs)
