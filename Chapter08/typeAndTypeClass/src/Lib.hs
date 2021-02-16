e7f9a578908c339c6e8b36cfd6305523b7772426
DenTaku <dentaku1412@gmail.com>
2021-02-16 22:57:02 +0900

    Done 8.1

--- Chapter08/typeAndTypeClass/src/Lib.hs
+++ Chapter08/typeAndTypeClass/src/Lib.hs
@@ -3,4 +3,16 @@ module Lib
     ) where
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+    print $ find "1" [("1", 1), ("2", 2), ("3", 3)]
+
+-- type String = [Char]
+
+type Pos a = (a, a)
+
+type Trans a = Pos a -> Pos a
+
+type Assoc k v = [(k,v)]
+
+find :: Eq k => k -> Assoc k v -> v
+find k t = head [v | (k',v) <- t, k == k']
