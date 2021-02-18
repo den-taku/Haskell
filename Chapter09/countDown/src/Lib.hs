1a35204aba6fe7e1f7f7962904c6ffb77836f1bc
DenTaku <dentaku1412@gmail.com>
2021-02-18 23:34:04 +0900

    Done 9.5

--- Chapter09/countDown/src/Lib.hs
+++ Chapter09/countDown/src/Lib.hs
@@ -5,6 +5,14 @@ module Lib
 someFunc :: IO ()
 someFunc = do
     print "Hello, world"
+    print $ show (App Add (Val 1) (App Mul (Val 2) (Val 3)))
+    print $ eval (App Add (Val 2) (Val 3))
+    print $ eval (App Sub (Val 2) (Val 3))
+    print $ subs [1, 2, 3]
+    print $ interleave 1 [2, 3, 4]
+    print $ perms [1, 2, 3]
+    print $ choices [1, 2, 3]
+    print $ solution (App Mul (App Add (Val 1) (Val 50)) (App Sub (Val 25) (Val 10))) [1, 3, 7, 10, 25, 50] 765
 
 data Op = Add | Sub | Mul | Div
 
@@ -13,3 +21,53 @@ instance Show Op where
     show Sub = "-"
     show Mul = "*"
     show Div = "/"
+
+valid :: Op -> Int -> Int -> Bool
+valid Add _ _ = True
+valid Sub x y = x > y
+valid Mul _ _ = True
+valid Div x y = x `mod` y == 0
+
+apply :: Op -> Int -> Int -> Int
+apply Add x y = x + y
+apply Sub x y = x - y
+apply Mul x y = x * y
+apply Div x y = x `div` y
+
+data Expr = Val Int | App Op Expr Expr
+
+instance Show Expr where
+    show (Val n)     = show n
+    show (App o l r) = brak l ++ show o ++ brak r
+                       where
+                           brak (Val n) = show n
+                           brak e       = "(" ++ show e ++ ")"
+
+values :: Expr -> [Int]
+values (Val n)     = [n]
+values (App _ l r) = values l ++ values r
+
+eval :: Expr -> [Int]
+eval (Val n)     = [n | n > 0]
+eval (App o l r) = [apply o x y | x <- eval l,
+                                  y <- eval r,
+                                  valid o x y]
+
+subs :: [a] -> [[a]]
+subs []     = [[]]
+subs (x:xs) = yss ++ map (x:) yss
+              where yss = subs xs
+
+interleave :: a -> [a] -> [[a]]
+interleave x []     = [[x]]
+interleave x (y:ys) = (x:y:ys) : map (y:) (interleave x ys)
+
+perms :: [a] -> [[a]]
+perms []     = [[]]
+perms (x:xs) = concat (map (interleave x) (perms xs))
+
+choices :: [a] -> [[a]]
+choices = concat . map perms . subs
+
+solution :: Expr -> [Int] -> Int -> Bool
+solution e ns n = elem (values e) (choices ns) && eval e == [n]
