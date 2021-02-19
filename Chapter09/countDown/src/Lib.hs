92569e56b929d3b59f19cec0e89cb8892fee4251
DenTaku <dentaku1412@gmail.com>
2021-02-19 21:49:25 +0900

    Done 9.7

--- Chapter09/countDown/src/Lib.hs
+++ Chapter09/countDown/src/Lib.hs
@@ -16,6 +16,8 @@ someFunc = do
     print $ split [1,2,3,4]
     -- print $ solutions [1, 3, 7, 10, 25, 50] 765
     -- print $ solutions [1, 3, 7, 10, 25, 50] 831
+    print $ solutions' [1, 3, 7, 10, 25, 50] 765
+    -- print $ solutions' [1, 3, 7, 10, 25, 50] 831
 
 data Op = Add | Sub | Mul | Div
 
@@ -96,3 +98,19 @@ ops = [Add,Sub,Mul,Div]
 
 solutions :: [Int] -> Int -> [Expr]
 solutions ns n = [e | ns' <- choices ns, e <- exprs ns', eval e == [n]]
+
+type Result = (Expr,Int)
+
+results :: [Int] -> [Result]
+results []  = []
+results [n] = [(Val n,n) | n > 0]
+results ns  = [res | (ls,rs) <- split ns,
+                     lx      <- results ls,
+                     ry      <- results rs,
+                     res     <- combine' lx ry ]
+                    
+combine' :: Result -> Result -> [Result]
+combine' (l,x) (r,y) = [(App o l r, apply o x y) | o <- ops, valid o x y] 
+
+solution' :: [Int] -> Int -> [Expr]
+solution' ns n = [e | ns' <- choices ns, (e,m) <- results ns', m == n]
