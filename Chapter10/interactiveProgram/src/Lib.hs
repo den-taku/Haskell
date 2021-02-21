f774de405db91c9ed8c28dae273c309fe0f96d8a
DenTaku <dentaku1412@gmail.com>
2021-02-21 23:31:59 +0900

    Learn basis of IO

--- Chapter10/interactiveProgram/src/Lib.hs
+++ Chapter10/interactiveProgram/src/Lib.hs
@@ -3,4 +3,39 @@ module Lib
     ) where
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = strlen
+
+act :: IO (Char,Char)
+act = do 
+    x <- getChar
+    getChar
+    y <- getChar
+    return (x,y)
+
+getLine :: IO String
+getLine = do 
+    x <- getChar
+    if x == '\n' then
+        return []
+    else 
+        do xs <- Lib.getLine
+           return (x:xs)
+          
+putStr :: String -> IO ()
+putStr []     = return ()
+putStr (x:xs) = do
+    putChar x
+    Lib.putStr xs
+
+putStrLn :: String -> IO ()
+putStrLn xs = do
+    Lib.putStr xs
+    putChar '\n'
+
+strlen :: IO ()
+strlen = do 
+    Lib.putStr "Enter a string: "
+    xs <- Lib.getLine
+    Lib.putStr "The string has "
+    Lib.putStr (show (length xs))
+    Lib.putStrLn " characters"
