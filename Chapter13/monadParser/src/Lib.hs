6e60b9c3d157ae8313374f9379096d1dab7200fa
DenTaku <dentaku1412@gmail.com>
2021-04-02 00:56:06 +0900

    Implement some parser

--- Chapter13/monadParser/src/Lib.hs
+++ Chapter13/monadParser/src/Lib.hs
@@ -14,6 +14,19 @@ someFunc = do
     print $ parse (pure 1) "abc"
     print $ parse three "abcdef"
     print $ parse three "ab"
+    print $ parse (empty :: Parser Char) "abc"
+    print $ parse (item <|> return 'd') "abc"
+    print $ parse ((P (\inp -> [])) <|> return 'd') "abc"
+    print $ parse (char 'a') "abc"
+    print $ parse (string "abc") "abcdef"
+    print $ parse (string "abc") "ab1234"
+    print $ parse (many digit) "123abc"
+    print $ parse (many digit) "abc"
+    print $ parse (some digit) "abc"
+    print $ parse ident "abc def"
+    print $ parse nat "123 abc"
+    print $ parse space "   abc"
+    print $ parse int "-123 abc"  
     return ()
 
 newtype Parser a = P (String -> [(a,String)])
@@ -58,3 +71,65 @@ three = do
     item
     z <- item
     return (x,z)
+
+instance Alternative Parser where
+    -- empty :: Parser a
+    empty = P (\inp -> [])
+
+    -- (<|>) :: Parser a -> Parser a -> Parser a
+    p <|> q = P (\inp -> case parse p inp of
+        []        -> parse q inp
+        [(v,out)] -> [(v,out)])
+
+sat :: (Char -> Bool) -> Parser Char
+sat p = do 
+    x <- item
+    if p x then return x else empty
+
+digit :: Parser Char
+digit = sat isDigit
+
+lower :: Parser Char
+lower = sat isLower
+
+upper :: Parser Char
+upper = sat isUpper
+
+letter :: Parser Char
+letter = sat isAlpha
+
+alphanum :: Parser Char
+alphanum = sat isAlphaNum
+
+char :: Char -> Parser Char
+char x = sat (== x)
+
+string :: String -> Parser String
+string []     = return []
+string (x:xs) = do
+    char x
+    string xs
+    return (x:xs)
+
+ident :: Parser String
+ident = do
+    x <- lower
+    xs <- many alphanum
+    return (x:xs)
+
+nat :: Parser Int
+nat = do
+    xs <- some digit
+    return (read xs)
+
+space :: Parser ()
+space = do
+    many (sat isSpace)
+    return ()
+
+int :: Parser Int
+int = do
+    char '-'
+    n <- nat
+    return (-n)
+  <|> nat
