baca9e9d416512b8ded09160d151b821bca93f29
DenTaku <dentaku1412@gmail.com>
2021-04-05 16:38:07 +0900

    Learn ~getProduct

--- Chapter14/foldableAndTraversable/src/Lib.hs
+++ Chapter14/foldableAndTraversable/src/Lib.hs
@@ -4,6 +4,7 @@ module Lib
 
 import Data.Typeable
 import Data.Monoid
+import Data.Foldable
 
 someFunc :: IO ()
 someFunc = do
@@ -19,3 +20,42 @@ someFunc = do
     print $ getAny $ mconcat [Any False, Any False, Any False]
     print $ getAll $ All True <> All True <> All True
     print $ [1, 2, 3] `mappend` [3, 4] `mappend` mempty `mappend` []
+    print $ fold [All True, All True, All True, All False]
+    print $ Lib.foldMap' id [All True, All True, All True, All False]
+    print $ getSum (foldMap Sum [1..10])
+    print $ getProduct (foldMap Product [1..10])
+
+
+-- fold :: Monoid a => [a] -> a
+-- fold []     = mempty
+-- fold (x:xs) = x `mappend` fold xs
+
+-- data Tree a = Leaf a | Node (Tree a) (Tree a) deriving Show
+
+-- foldt :: Monoid a => Tree a -> a
+-- foldt (Leaf x)   = x
+-- foldt (Node l r) = fold l `mappend` fold r
+
+class Foldable' t where
+       fold'    :: Monoid a => t a -> a
+       foldMap' :: Monoid b => (a -> b) -> t a -> b
+       foldr'   :: (a -> b -> b) -> b -> t a -> b
+       foldl'   :: (a -> b -> a) -> a -> t b -> a
+
+instance Foldable' [] where
+    -- fold :: Monoid a => [a] -> a
+    fold' []     = mempty
+    fold' (x:xs) = x `mappend` Lib.fold' xs
+
+    -- foldMap :: Monoid b => (a -> b) -> [a] -> b
+    foldMap' _ []     = mempty
+    foldMap' f (x:xs) = f x `mappend` Lib.foldMap' f xs
+
+    -- foldr :: (a -> b -> b) -> b -> [a] -> b
+    foldr' _ v []     = v
+    foldr' f v (x:xs) = f x (Lib.foldr' f v xs)
+
+    -- foldl :: (a -> b -> a) -> a -> [b] -> a
+    foldl' _ v []     = v
+    foldl' f v (x:xs) = Lib.foldl' f (f v x) xs
+
