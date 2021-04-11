3fd23e47e88feae40806e12a5791d07ecb5714b1
DenTaku <dentaku1412@gmail.com>
2021-04-11 22:31:33 +0900

    Implement aplit

--- Chapter15/lazyEvaluation/src/Lib.hs
+++ Chapter15/lazyEvaluation/src/Lib.hs
@@ -6,6 +6,14 @@ someFunc :: IO ()
 someFunc = do
     print "someFunc"
     print $ inc (2*3)
+    print $ split [1,2,3,4]
+    print $ split [1,2,3,4,5]
+    print $ split [3]
 
 inc :: Int -> Int
 inc = (+) 1
+
+split :: [a] -> ([a], [a])
+split [] = ([],[])
+split [x] = ([x], [])
+split (x:y:zs) = (x:xs,y:ys) where (xs,ys) = split zs
